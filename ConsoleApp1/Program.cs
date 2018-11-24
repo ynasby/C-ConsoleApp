7651a5109ea3d7f18c5444394c98ee9a160f2ada
ynasby <45235767+ynasby@users.noreply.github.com>
2018-11-25 01:23:02 +0400

    Арифметика

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApp1
             string doubleNumber = "894376.243643";
             int number = (int)double.Parse(doubleNumber, CultureInfo.InvariantCulture); // Вася уверен, что ошибка где-то тут
             Console.WriteLine(number + 1);
+
+            //var a = 2;
+            //var b = a++ + ++a;
+
+            var a = 5.0; // ← исправьте эту строку
+            a += 0.5;
+            Console.WriteLine(a);
+
         }
     }
 }
